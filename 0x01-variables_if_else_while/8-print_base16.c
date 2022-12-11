f89ea2a48ffb38657602f8eface4568971945489
Amanuel <amiyeic@gcomcom>
2022-12-11 02:19:43 -0800

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet in reversed order
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 122; i > 96; i--)
+{
+putchar(i);
+}
+for (i = 97; i< 103; i++)
+{
+putchar(i)
+}
+putchar('\n');
+return (0);
+}
