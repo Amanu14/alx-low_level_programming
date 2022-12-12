7ecdf2835f25e0a94afbb71a0fe027856d10fda0
Amanuel <amiyeic@gcomcom>
2022-12-12 11:26:54 -0800

    Print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,42 +7,42 @@
  */
 int main(void)
 {
-  int i, x, y, z, c1, c2;
+int i, x, y, z, c1, c2;
 
-  i = x = y = z = 48;
-  while (z < 58)
-    {
-      y = 48;
-      while (y < 58)
-	{
-	  x = 48;
-	  while (x < 58)
-	    {
-	      i = 48;
-	      while (i < 58)
-		{
-		  c1 = (z * 10) + y;
-		  c2 = (x * 10) + i;
-		  if (c1 < c2)
-		    {
-		      putchar(z);
-		      putchar(y);
-		      putchar(' ');
-		      putchar(x);
-		      putchar(i);
-		      if (z == 57 && y == 56 && x == 57 && i == 57)
-			break;
-		      putchar(',');
-		      putchar(' ');
-		    }
-		  i++;
-		}
-	      x++;
-	    }
-	  y++;
-	}
-      z++;
-    }
-  putchar('\n');
-  return (0);
+i = x = y = z = 48;
+while (z < 58)
+{
+y = 48;
+while (y < 58)
+{
+x = 48;
+while (x < 58)
+{
+i = 48;
+while (i < 58)
+{
+c1 = (z * 10) + y;
+c2 = (x * 10) + i;
+if (c1 < c2)
+{
+putchar(z);
+putchar(y);
+putchar(' ');
+putchar(x);
+putchar(i);
+if (z == 57 && y == 56 && x == 57 && i == 57)
+break;
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+x++;
+}
+y++;
+}
+z++;
+}
+putchar('\n');
+return (0);
 }
