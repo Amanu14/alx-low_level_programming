8da034aea9268bd2ab3c20d7cf3245cb81202f7e
Amanuel <amiyeic@gcomcom>
2022-12-13 13:15:30 -0800

    101 natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * mainr - Prints sum of multiples of 3 and 5
+ *
+ * Return: no return
+ */
+ 
+void main(void)
+
+{
+int a;
+int sum=0;
+  
+for (a = 1; a < 1024; a++)
+{
+if ((a % 3) == 0 || (a % 5) == 0)
+{
+sum =sum +a;
+}
+printf("%d", sum);
+printf("\n");
+
+}
+}
