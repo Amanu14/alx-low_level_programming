627f1301f23d9c89191173c0536c049ea75a6127
Amanuel <amiyeic@gcomcom>
2022-12-13 13:26:06 -0800

    101 natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,17 +5,17 @@
  *
  * Return: no return
  */
- 
+
 int main(void)
 
 {
 int a;
-int sum=0;
+int sum = 0;
   
 for (a = 1; a < 1024; a++)
 {
 if ((a % 3) == 0 || (a % 5) == 0)
-sum =sum +a;
+sum = sum + a;
 }
 printf("%d", sum);
 printf("\n");
