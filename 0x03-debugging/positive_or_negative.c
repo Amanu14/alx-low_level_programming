bb3912da78f012499a955f3fb90f86bb460822cc
Amanuel <amiyeic@gcomcom>
2022-12-15 09:39:29 -0800

    positive negative

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+void positive_or_negative(int i)
+{
+  
+  /* your code goes there */
+  if (i > 0)
+    {
+      printf("%d is positive\n", i);
+    }
+  else if (i == 0)
+    {
+      printf("%d is zero\n", i);
+    }
+  else
+    {
+      printf("%d is negative\n", i);
+    }
+  return (0);
+}
