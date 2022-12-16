753d25b3fe16bf4aecc495e912b609b0cc068bbc
Amanuel <amiyeic@gcomcom>
2022-12-16 12:58:49 -0800

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if parameter is an uppercase character.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
