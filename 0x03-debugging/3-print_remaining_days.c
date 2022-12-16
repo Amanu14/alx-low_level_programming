b7d18f8fa281854a2a907d483ed2a2a78d013620
Amanuel <amiyeic@gcomcom>
2022-12-16 12:07:06 -0800

    conver day

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_remaining_days - takes a date and prints how many days are
@@ -25,7 +25,7 @@ printf("Remaining days: %d\n", 366 - day);
 else
 {
 if (month == 2 && day == 60)
-
+{
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
