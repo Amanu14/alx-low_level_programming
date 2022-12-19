46c26ac0ec1b6660bbc38d9aace1cb48274aa5fe
Amanuel <amiyeic@gcomcom>
2022-12-19 15:26:59 -0800

    print half the string

--- 0x05-pointers_arrays_strings/7-puts2.c
+++ 0x05-pointers_arrays_strings/7-puts2.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * puts_half - prints half of a string.
  * @str: input string.
@@ -21,6 +21,6 @@ else
 i = (count - 1) / 2;
 
 for (i++; i < count; i++)
-_putchar(str[i]);
-_putchar('\n');
+putchar(str[i]);
+putchar('\n');
 }
